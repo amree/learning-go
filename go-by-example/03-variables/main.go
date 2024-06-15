6196451bd6efe6b6b432a6de3b23409b48701023
Amree Zaid <mohd.amree@gmail.com>
2024-06-15 15:17:41 +0800

    Practise Go by Example

---
+++ go-by-example/03-variables/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := "initial"
+	fmt.Println(a)
+
+	var b, c int = 1, 2
+	fmt.Println(b, c)
+
+	d := true
+	fmt.Println(d)
+
+	var e int
+	fmt.Println(e)
+
+	f := "apple"
+	fmt.Println(f)
+}
