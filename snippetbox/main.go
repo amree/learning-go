1fbe366238e299eb85846862d93d7289dcbf9079
Amree Zaid <mohd.amree@gmail.com>
2024-06-16 16:30:37 +0800

    Practise Let's Go - Chapter 2.3

--- snippetbox/main.go
+++ snippetbox/main.go
@@ -11,11 +11,21 @@ func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello from Snippetbox"))
 }
 
+func snippetView(w http.ResponseWriter, _ *http.Request) {
+	w.Write([]byte("Display a specific snippet..."))
+}
+
+func snippetCreate(w http.ResponseWriter, _ *http.Request) {
+	w.Write([]byte("Create a new snippet..."))
+}
+
 func main() {
 	// Initialize new new servemux
 	// Register the home function as the handler for the "/" URL pattern
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
+	mux.HandleFunc("/{$}", home)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
 
 	// Print a log message to say the server is starting
 	log.Print("Starting server on :4000")
