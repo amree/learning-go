4a4f5edf2e829fd04d45ad9688d10e5b496856a1
Amree Zaid <mohd.amree@gmail.com>
2024-06-21 23:54:27 +0800

    Practise Let's Go - Chapter 3.5 - Isolating the application routes

--- snippetbox/cmd/web/main.go
+++ snippetbox/cmd/web/main.go
@@ -23,21 +23,6 @@ func main() {
 		logger: logger,
 	}
 
-	// Initialize new new servemux
-	// Register the home function as the handler for the "/" URL pattern
-	mux := http.NewServeMux()
-
-	// Create file server which serves files from the "./ui/static" directory
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-
-	// We strip the "/static" prefix from the URL path before the request reaches the file server
-	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
-
-	mux.HandleFunc("GET /{$}", app.home)
-	mux.HandleFunc("GET /snippet/view/{id}", app.snippetView)
-	mux.HandleFunc("GET /snippet/create", app.snippetCreate)
-	mux.HandleFunc("POST /snippet/create", app.snippetCreatePost)
-
 	// Print a log message to say the server is starting
 	logger.Info("starting server", slog.Any("addr", *addr))
 
@@ -45,7 +30,7 @@ func main() {
 	// 1. The TCP network address to listen to
 	// 2. The servemux to use
 	// Pass the deferenced addr pointer
-	err := http.ListenAndServe(*addr, mux)
+	err := http.ListenAndServe(*addr, app.routes())
 
 	logger.Error(err.Error())
 	os.Exit(1)
