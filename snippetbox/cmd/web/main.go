d8b3199a2e1a7ed73c84114fc51cb3faa3adf4f8
Amree Zaid <mohd.amree@gmail.com>
2024-06-22 14:36:24 +0800

    Practise Let's Go - Chapter 4.4 - Creating a database connection pool

--- snippetbox/cmd/web/main.go
+++ snippetbox/cmd/web/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log/slog"
 	"net/http"
 	"os"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type application struct {
@@ -14,10 +17,19 @@ type application struct {
 func main() {
 	// Commnad-line flag name=addr, default value=:4000 and help message
 	addr := flag.String("addr", ":4000", "HTTP network address")
+	dsn := flag.String("dsn", "web:pass@/snippetbox?parseTime=true", "MySQL data source name")
 	flag.Parse()
 
 	// Initialize a new structured logger that writes to standard output stream and uses the default settings
 	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+
+	db, err := openDB(*dsn)
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+
+	defer db.Close()
 
 	app := &application{
 		logger: logger,
@@ -30,8 +42,23 @@ func main() {
 	// 1. The TCP network address to listen to
 	// 2. The servemux to use
 	// Pass the deferenced addr pointer
-	err := http.ListenAndServe(*addr, app.routes())
+	err = http.ListenAndServe(*addr, app.routes())
 
 	logger.Error(err.Error())
 	os.Exit(1)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		db.Close()
+		return nil, err
+	}
+
+	return db, nil
 }
