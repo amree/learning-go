a183a62c4115a64782215db270d2447ac6b5f1c5
Amree Zaid <mohd.amree@gmail.com>
2024-06-30 22:48:41 +0800

    Practise Let's Go - Chapter 5.4 - Catching runtime errors

--- lets-go/cmd/web/templates.go
+++ lets-go/cmd/web/templates.go
@@ -28,7 +28,7 @@ func newTemplateCache() (map[string]*template.Template, error) {
 			return nil, err
 		}
 
-		ts, err = ts.ParseGlob("./ui/html/pages/*.tmpl")
+		ts, err = ts.ParseGlob("./ui/html/partials/*.tmpl")
 		if err != nil {
 			return nil, err
 		}
