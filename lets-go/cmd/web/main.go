2fb7827c9f98405f161a44106dc701447ecc0877
Amree Zaid <mohd.amree@gmail.com>
2024-11-15 18:19:15 +0800

    Practise Let's Go - Chapter 9.6 - Connection timeouts

--- lets-go/cmd/web/main.go
+++ lets-go/cmd/web/main.go
@@ -79,10 +79,13 @@ func main() {
 	}
 
 	srv := &http.Server{
-		Addr:      *addr,
-		Handler:   app.routes(),
-		ErrorLog:  slog.NewLogLogger(logger.Handler(), slog.LevelError),
-		TLSConfig: tlsConfig,
+		Addr:         *addr,
+		Handler:      app.routes(),
+		ErrorLog:     slog.NewLogLogger(logger.Handler(), slog.LevelError),
+		TLSConfig:    tlsConfig,
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
 	}
 
 	// Print a log message to say the server is starting
