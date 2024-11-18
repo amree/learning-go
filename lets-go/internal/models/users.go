e7acb2be420b2bd1ce657c85f0ea071a29398725
Amree Zaid <mohd.amree@gmail.com>
2024-11-18 23:12:30 +0800

    Practise Let's Go - Chapter 11.2 - Request context for authentication/authorization

--- lets-go/internal/models/users.go
+++ lets-go/internal/models/users.go
@@ -74,5 +74,10 @@ func (m *UserModel) Authenticate(email, password string) (int, error) {
 }
 
 func (m *UserModel) Exists(id int) (bool, error) {
-	return false, nil
+	var exists bool
+
+	stmt := "SELECT EXISTS(SELECT 1 FROM users WHERE id = ?)"
+
+	err := m.DB.QueryRow(stmt, id).Scan(&exists)
+	return exists, err
 }
