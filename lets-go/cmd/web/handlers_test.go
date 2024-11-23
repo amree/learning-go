ce60fab09eb13ff03e59c01f4f2b78d4bbb653a5
Amree Zaid <mohd.amree@gmail.com>
2024-11-23 23:15:32 +0800

    Practise Let's Go - Chapter 13.5 - Mocking dependencies

--- lets-go/cmd/web/handlers_test.go
+++ lets-go/cmd/web/handlers_test.go
@@ -13,8 +13,53 @@ func TestPing(t *testing.T) {
 	ts := newTestServer(t, app.routes())
 	defer ts.Close()
 
-	code, _, body := ts.get(t, "/ping")
+	tests := []struct {
+		name     string
+		urlPath  string
+		wantCode int
+		wantBody string
+	}{
+		{
+			name:     "Valid ID",
+			urlPath:  "/snippet/view/1",
+			wantCode: http.StatusOK,
+			wantBody: "An old silent pond...",
+		},
+		{
+			name:     "Non-existent ID",
+			urlPath:  "/snippet/view/2",
+			wantCode: http.StatusNotFound,
+		},
+		{
+			name:     "Negative ID",
+			urlPath:  "/snippet/view/-1",
+			wantCode: http.StatusNotFound,
+		},
+		{
+			name:     "Decimal ID",
+			urlPath:  "/snippet/view/1.23",
+			wantCode: http.StatusNotFound,
+		},
+		{
+			name:     "String ID",
+			urlPath:  "/snippet/view/one",
+			wantCode: http.StatusNotFound,
+		},
+		{
+			name:     "Empty ID",
+			urlPath:  "/snippet/view/",
+			wantCode: http.StatusNotFound,
+		},
+	}
 
-	assert.Equal(t, code, http.StatusOK)
-	assert.Equal(t, body, "OK")
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			code, _, body := ts.get(t, tt.urlPath)
+			assert.Equal(t, code, tt.wantCode)
+			if tt.wantBody !=
+				"" {
+				assert.StringContains(t, body, tt.wantBody)
+			}
+		})
+	}
 }
--- lets-go/internal/assert/assert.go
+++ lets-go/internal/assert/assert.go
@@ -1,11 +1,22 @@
 package assert
 
-import "testing"
+import (
+	"strings"
+	"testing"
+)
 
 func Equal[T comparable](t *testing.T, actual, expected T) {
 	t.Helper()
 
 	if actual != expected {
 		t.Errorf("expected %v but got %v", expected, actual)
+	}
+}
+
+func StringContains(t *testing.T, actual, expectedSubstring string) {
+	t.Helper()
+
+	if !strings.Contains(actual, expectedSubstring) {
+		t.Errorf("expected %q to contain %q", actual, expectedSubstring)
 	}
 }
